4bfab009559a42c83276d99b0554cb14c100592d
baker615512 <baker615512@gmail.com>
2021-01-27 20:26:09 -0600

    setting up flavors controller

--- coffee_notes_api/app/controllers/api/v1/flavors_controller.rb
+++ coffee_notes_api/app/controllers/api/v1/flavors_controller.rb
@@ -1,2 +1,28 @@
 class API::V1::FlavorsController < ApplicationController
+
+  def index
+    @flavors = Flavor.all
+    render json: @flavors
+  end
+
+  def create
+    @flavor = Flavor.new(flavor_params)
+    if @flavor.save
+      render json: @flavor
+    else
+      render json: @flavor.errors
+    end
+  end
+
+  def show
+    @flavor = Flavor.find(params[:id])
+    render json: @flavor
+  end
+
+  private
+
+  def flavor_params
+    params.require(:flavor).permit(:name, :votes, :country_id)
+  end
+  
 end
