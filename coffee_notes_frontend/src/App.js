83ac2e1829eb42745a70fb004df724406f6c65d0
baker615512 <baker615512@gmail.com>
2021-01-29 22:48:42 -0600

    setting up routes

--- coffee_notes_frontend/src/App.js
+++ coffee_notes_frontend/src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import CountriesContainer from './containers/CountriesContainer'
 
 class App extends Component {
+
+  function Home() {
+    return (
+      <div>
+        <h2>Home</h2>
+      </div>
+    );
+  }
 
   render(){
     return (
       <Router>
         <div>
           <Switch>
-            <Route exact path="/" component={Home}/>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/countries" component={CountriesContainer}/>
           </Switch>
         </div>
       </Router>
---
+++ coffee_notes_frontend/src/containers/CountriesContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class CountriesContainer extends Component {
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default CountriesContainer
