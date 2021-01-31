e4be584986b630cff086e3b0810d326024e0a7b7
baker615512 <baker615512@gmail.com>
2021-01-30 20:51:13 -0600

    removing unused actions in flavors controller

--- coffee_notes_api/app/controllers/api/v1/flavors_controller.rb
+++ coffee_notes_api/app/controllers/api/v1/flavors_controller.rb
@@ -1,12 +1,7 @@
 class Api::V1::FlavorsController < ApplicationController
-  before_action :load_country
-
-  def index
-    render json: @country.flavors
-  end
 
   def create
-    @flavor = @country.flavor.new(flavor_params)
+    @flavor = flavor.new(flavor_params)
     if @flavor.save
       render json: @flavor
     else
@@ -14,19 +9,10 @@ class Api::V1::FlavorsController < ApplicationController
     end
   end
 
-  def show
-    @flavor = @country.flavor.find(params[:id])
-    render json: @flavor
-  end
-
   private
 
   def flavor_params
-    params.require(:flavor).permit(:name, :votes, :country_id)
-  end
-
-  def load_country
-    @country = Country.find(params[:country_id])
+    params.require(:flavor).permit(:name)
   end
 
 end
