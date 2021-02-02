8b152e46a91f2c3b3f09ffed031c9bc98c1962e4
baker615512 <baker615512@gmail.com>
2021-02-01 21:47:45 -0600

    wip: moving fetch countries and connect to countries container

--- coffee_notes_frontend/src/App.js
+++ coffee_notes_frontend/src/App.js
@@ -1,44 +1,18 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import CountriesContainer from './containers/CountriesContainer'
-import { connect } from 'react-redux'
-import { fetchCountries } from './redux/actionCreator'
 
 class App extends Component {
-
-  componentDidMount(){
-    this.props.fetchCountries()
-  }
 
   render(){
     return (
       <Router>
-        <div>
           <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/countries" component={CountriesContainer}/>
+            <Route exact path="/" component={CountriesContainer} />
           </Switch>
-        </div>
       </Router>
     );
   }
 }
 
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-const mapStateToProps = state => {
-  return {
-    countryPics: state.countries,
-    loading: state.loading
-  }
-}
-
-export default connect(mapStateToProps, {fetchCountries})(App);
+export default App;
--- coffee_notes_frontend/src/containers/CountriesContainer.js
+++ coffee_notes_frontend/src/containers/CountriesContainer.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchCountries } from '../redux/actionCreator'
 
 class CountriesContainer extends Component {
+
+  componentDidMount(){
+    this.props.fetchCountries()
+  }
+
   render(){
     return(
       <div></div>
@@ -8,4 +15,11 @@ class CountriesContainer extends Component {
   }
 }
 
-export default CountriesContainer
+const mapStateToProps = state => {
+  return {
+    countryPics: state.countries,
+    loading: state.loading
+  }
+}
+
+export default connect(mapStateToProps, {fetchCountries})(CountriesContainer)
