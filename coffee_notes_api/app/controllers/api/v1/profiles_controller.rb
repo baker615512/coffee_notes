5e148e014126345a3812203991c0e3dc429f0697
baker615512 <baker615512@gmail.com>
2021-02-04 19:44:27 -0600

    updating profile vote count

--- coffee_notes_api/app/controllers/api/v1/profiles_controller.rb
+++ coffee_notes_api/app/controllers/api/v1/profiles_controller.rb
@@ -4,4 +4,11 @@ class Api::V1::ProfilesController < ApplicationController
     @profiles = Country.find(params[:country_id]).profiles
     render json: @profiles
   end
+
+  def update
+    @profile = Profile.find(params[:id])
+    @profile.votes = @profile.votes + 1
+    @profile.save
+    render json: @profile
+  end
 end
--- coffee_notes_api/config/routes.rb
+++ coffee_notes_api/config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :countries, only: [:index, :show] do
         resources :flavors, only: [:index, :create]
-        resources :profiles, only: [:index, :show, :update]
+        resources :profiles, only: [:index, :show]
       end
-      
+      resources :profiles, only: [:update]
       resources :flavors, only: [:index, :show] # stretch goal
     end
   end
