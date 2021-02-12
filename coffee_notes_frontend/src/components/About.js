6a67122e4a6f40f1be1469f07f9b9d28ac697f5a
baker615512 <baker615512@gmail.com>
2021-02-11 21:26:10 -0600

    adding button link on about page

--- coffee_notes_frontend/src/components/About.js
+++ coffee_notes_frontend/src/components/About.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
@@ -9,6 +11,9 @@ const About = () => {
       <h1>Coffee Notes</h1>
       <h5>Welcome to a place where you can share your coffee-tasting experiences.</h5>
       <h5>Vote on flavor profiles of coffee from various countries.</h5>
+      <Link to="/countries">
+        <Button variant="info">See Countries</Button>
+      </Link>
     </Container>
   </Jumbotron>
   )
