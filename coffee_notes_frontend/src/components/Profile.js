ca7bea19c572b2bd20bab0c89423085d1ce75083
baker615512 <baker615512@gmail.com>
2021-02-12 12:29:21 -0600

    changing profile to class component

--- coffee_notes_frontend/src/components/Profile.js
+++ coffee_notes_frontend/src/components/Profile.js
@@ -4,25 +4,27 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import Button from 'react-bootstrap/Button';
 import { connect } from 'react-redux'
 import { upvoteProfile } from '../redux/actionCreator'
+import { Component } from 'react';
 
-const Profile = props => {
+class Profile extends Component {
 
-  const handleOnClick = () => {
-    props.upvoteProfile(props.id)
+  handleOnClick = () => {
+    this.props.upvoteProfile(this.props.id)
   }
 
+  render(){
   return(
     <Card style={{ width: '28rem' }}>
       <Card.Body>
       <ListGroup variant="flush">
-        <ListGroup.Item>{props.flavor}</ListGroup.Item>
-        <ListGroup.Item>Vote count: {props.votes}</ListGroup.Item>
-        <Button variant="info" size="lg" id={props.id} 
-        onClick={handleOnClick}>Vote</Button>{' '}
+        <ListGroup.Item>{this.props.flavor}</ListGroup.Item>
+        <ListGroup.Item>Vote count: {this.props.votes}</ListGroup.Item>
+        <Button variant="info" size="lg" id={this.props.id} 
+        onClick={this.handleOnClick}>Vote</Button>{' '}
       </ListGroup>
       </Card.Body>
     </Card>
-  )
+  )}
 }
 
 export default connect(null, {upvoteProfile})(Profile)
